43d85099c98e9c1a45e348b7af98cc9ffd297fb7
Caglayan Serbetci <caglayanserbetci.de@gmail.com>
2022-11-14 21:24:38 +0300

    feat: add mongoose to project

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordFactoryModule } from './record-factory/record-factory.module';
 
 @Module({
-  imports: [RecordFactoryModule],
+  imports: [RecordFactoryModule, MongooseModule.forRoot('mongodb://localhost:27017'),],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
