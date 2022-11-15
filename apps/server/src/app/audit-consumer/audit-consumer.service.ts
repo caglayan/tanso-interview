4f6a96888d77364675aa5c82bfee1e552d788ef9
Caglayan Serbetci <caglayanserbetci.de@gmail.com>
2022-11-15 12:58:53 +0300

    chore: remove unneeded imports

--- apps/server/src/app/audit-consumer/audit-consumer.service.ts
+++ apps/server/src/app/audit-consumer/audit-consumer.service.ts
@@ -1,6 +1,5 @@
 import { Job } from 'bull';
 import { Processor, Process } from '@nestjs/bull';
-import { Record } from '../record-factory/schemas/record.schema';
 import { AuditFactoryService } from '../audit-factory/audit-factory.service';
 
 @Processor('audit-queue')
